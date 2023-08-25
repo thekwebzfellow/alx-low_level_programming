386d0f5fc1c47aaabca8d56bc6600b218b89cb36
thekwebzfellow <mrkwebz@gmail.com>
2023-08-25 15:08:08 +0300

    First C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
 /**
+ * main - This is the entry point of the program
  *
- * main - This is the entry point of the code
- *
- * Always return 0 - successful;
- *
+ * Return: 0 is successful;
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle" '\n');
 
 	return (0);
 }
