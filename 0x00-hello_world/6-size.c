4406ddefc7c408eaa967b92b3ece44d84b6e3e67
thekwebzfellow <mrkwebz@gmail.com>
2023-08-25 17:52:33 +0300

    Data type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	printf("Size of a char: \n", sizeof(char));
-	printf("Size of an int: \n", sizeof(int));
-	printf("Size of a log int: \n", sizeof(long int));
-	printf("Size of a long long int: \n", sizeof(long long int));
-	printf("Size of a float: \n", sizeof(float));
+	printf("Size of a char: %c \n",sizeof(char));
+	printf("Size of an int: %d \n",sizeof(int));
+	printf("Size of a log int: %ld \n",sizeof(long int));
+	printf("Size of a long long int: %lld \n",sizeof(long long int));
+	printf("Size of a float: %f \n",sizeof(float));
 
 	return (0);
 }
