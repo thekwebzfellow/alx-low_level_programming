66b95f7f527f2729e42eef674d14eaf175280cea
thekwebzfellow <mrkwebz@gmail.com>
2023-08-25 13:56:32 +0300

    First C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints Programming is like building a multilingual puzzle
+ * main - prints "Programming is like building a multilingual puzzle"
  *
  * Always return 0;
  *
