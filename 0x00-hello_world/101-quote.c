3b145b82c9bd1d291743d5b6ea0e1064047058d0
thekwebzfellow <mrkwebz@gmail.com>
2023-08-27 11:42:54 +0300

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a statement to the standard error
+ *
+ * return: 1 to the standard error is success
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful"\"- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
